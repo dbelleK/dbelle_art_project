17285dd67b733a0f74ee9fa86aea80a5fb0cf54a
dbelleK <kkddy1206@naver.com>
2022-06-19 01:49:11 +0900

    no message

--- dbelleArt/src/main/java/com/dbellart/web/repository/UserMapper.java
+++ dbelleArt/src/main/java/com/dbellart/web/repository/UserMapper.java
@@ -33,30 +33,30 @@ import com.dbellart.web.domain.UserAuthority;
 
 	public interface UserMapper {
 		
-		//1. 회원가입 insert
+		//1. �쉶�썝媛��엯 insert
 		@Insert("INSERT INTO dbelle.user (name, email, pw, pwCheck, tel, address, checkAll) VALUES (#{name}, #{email}, #{pw}, #{pwCheck}, #{tel}, #{address} , #{checkAll})")
 		@Options(useGeneratedKeys = true, keyProperty = "userIdx")
-		void addUserinfo(Member ㄴㄷㄱuser);
+		void addUserinfo(Member user);
 		
 		//2.
 		@Select("select * from dbelle.user where email=#{email}")
 		Member joinUserInfo(String email);
 		
-		//3.이메일 중복검사
+		//3.�씠硫붿씪 以묐났寃��궗
 		@Select("select COUNT(email) from dbelle.user where email=#{email}")
 		int emailCheck(String email);
 		
-		//3. �α����ϱ�
+		//3. 占싸깍옙占쏙옙占싹깍옙
 		@Select("select * from dbelle.user where email=#{email} and pw=#{pw}")
 		Member loginUserInfo(Member user);
 		
-		//4. ���� ���� insert
+		//4. 占쏙옙占쏙옙 占쏙옙占쏙옙 insert
 		@Insert("INSERT INTO dbelle.authority(user_id,authority,created_date,modified_date) VALUES(#{userId}, #{authority}, NOW(), NOW())")
 		void insertAuthority(UserAuthority userAuthority);
 		 
-		//5. ��ϵ� ������ �����´�.
+		//5. 占쏙옙溝占� 占쏙옙占쏙옙占쏙옙 占쏙옙占쏙옙占승댐옙.
 	    @Select("SELECT * FROM dbelle.authority where user_id = #{asd}")
-	    List<UserAuthority> findAuthorityById(@Param("asd") Long ids); //ids=���� asd=������ ��
+	    List<UserAuthority> findAuthorityById(@Param("asd") Long ids); //ids=占쏙옙占쏙옙 asd=占쏙옙占쏙옙占쏙옙 占쏙옙
 
 
 
